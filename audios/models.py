917c78938178eec5baa18d0086c76d306999b85b
Steven Russell <stevenpaulrussell@gmail.com>
2021-11-05 12:15:34 -0700

    Changed url length from 100 t0 200. Works to play.

--- audios/models.py
+++ audios/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Audios(models.Model):
     text = models.TextField(default='no text entered')
-    url = models.CharField(max_length=100)
+    url = models.CharField(max_length=200)
     hint = models.CharField(max_length=30)
 
     def __str__(self) -> str:
