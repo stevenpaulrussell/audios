f220a3e27f1b73e48821a0a9f8c6f590ad9d7a04
Steven Russell <stevenpaulrussell@gmail.com>
2021-11-15 12:27:46 -0800

    Revert "Going back to simple button. Complicated branch should be a different site." NO ... FOUL UP WAS PUSHING TEST CHANGES TO HEROKU. SO, KEEP THIS AS THE TEST AND PULL A NEW SIMPLE ONE... This reverts commit 413ee7e188c2aa0be5b6afebab54dabc49558e5d.

--- audios/models.py
+++ audios/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 class Audios(models.Model):
+    from_tel = models.CharField(max_length=30, default='version1_default')
     text = models.TextField(default='no text entered')
     url = models.CharField(max_length=200)
     hint = models.CharField(max_length=30)
